10700517cb3586b9eb2ac96ef458904730a25fcf
albertoGF-dawt <albertoguerraforestier@alumno.ieselrincon.es>
2026-02-04 18:40:45 +0000

    asunto

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,9 +6,18 @@ function Header() {
                 <img src="logo.png" alt="logo" />
             </div>
             <div className="menu-header-optons">
+                <ul>
+                    <li>Inicio</li>
+                    <li>Menú</li>
+                    <li>Reclamaciones</li>
+                </ul>
 
             </div>
-            <div className="HEADER-HAMBURGUER"></div>
+            <div className="HEADER-HAMBURGUER">
+
+            </div>
         </div>
     )
 }
+
+export default Header
