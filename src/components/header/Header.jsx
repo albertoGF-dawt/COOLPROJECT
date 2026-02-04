d366dc2b674856bc66d0593a8c5e881565b07171
albertoGF-dawt <albertoguerraforestier@alumno.ieselrincon.es>
2026-02-04 18:27:46 +0000

    new Header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+
+        <div className="header-main">
+            <div className="header-logo">
+                <img src="logo.png" alt="logo" />
+            </div>
+            <div className="menu-header-optons">
+
+            </div>
+            <div className="HEADER-HAMBURGUER"></div>
+        </div>
+    )
+}
